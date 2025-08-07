f0f91f6b3b3fd81a57146cf41f2f64b3aa8b6964
soofiya <soofiyaayoob786@gmail.com>
2025-08-07 20:23:53 +0530

    nameconflis

--- src/components/LogoCarousal/LogoCarousal.jsx
+++ src/components/LogoCarousal/LogoCarousal.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./LogoCarousel.css";
 import dribbble from "../../assets/dribbble.svg";
-import awwwards from "../../assets/awwwards.svg";
+import awwwards from "../../assets/awards.svg";
 import behance from "../../assets/behance.svg";
 import cssDesign from "../../assets/cssdesign.svg";
 
